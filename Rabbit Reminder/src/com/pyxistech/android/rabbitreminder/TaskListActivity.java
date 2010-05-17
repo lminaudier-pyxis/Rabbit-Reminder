e0933793df48a6f6d8c15710eac72a55a66291b8
Loic Minaudier <lminaudier@pyxis-tech.com>
2010-05-17 16:27:14 +0200

    Initial commit

---
+++ Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/TaskListActivity.java
@@ -0,0 +1,13 @@
+package com.pyxistech.android.rabbitreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaskListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
