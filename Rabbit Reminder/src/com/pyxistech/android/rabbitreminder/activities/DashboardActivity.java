8457b8db289f8aae81ebf7b07830deecd6a01bea
Loic GIRON DIT METAZ <loic.gironditmetaz@gmail.com>
2010-05-27 15:18:07 +0200

    Added a basic blank dashboard

---
+++ Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/activities/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.pyxistech.android.rabbitreminder.activities;
+
+import com.pyxistech.android.rabbitreminder.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DashboardActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.dashboard);
+    }
+}
