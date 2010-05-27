47f0e4409cded9276f33df2bad7e9d791ea8ac29
lgironditmetaz <lgironditmetaz@pyxis-tech.com>
2010-05-27 22:32:35 +0200

    TaskListActivity can be launched from dashboard

--- Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/activities/DashboardActivity.java
+++ Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/activities/DashboardActivity.java
@@ -1,14 +1,28 @@
 package com.pyxistech.android.rabbitreminder.activities;
 
-import com.pyxistech.android.rabbitreminder.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.pyxistech.android.rabbitreminder.R;
 
 public class DashboardActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.dashboard);
+    	
+    	Button listsButton = (Button) findViewById(R.id.dashboard_lists_button);
+    	listsButton.setOnClickListener(listsClickListener);
     }
+    
+    private OnClickListener listsClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			Intent i = new Intent(DashboardActivity.this, TaskListActivity.class);
+			startActivity(i);
+		}
+	};
 }
