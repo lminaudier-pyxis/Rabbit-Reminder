b3638bd016333dc8fe76d27968388289fcd544fc
Loic GIRON DIT METAZ <lgironditmetaz@pyxis-tech.com>
2010-06-15 16:49:56 +0200

    Refactored AddTaskActivity

--- Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/activities/AddTaskActivity.java
+++ Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/activities/AddTaskActivity.java
@@ -18,6 +18,7 @@ import com.pyxistech.android.rabbitreminder.R;
 import com.pyxistech.android.rabbitreminder.models.TaskItem;
 
 public class AddTaskActivity extends Activity implements LocationListener {
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,129 +26,26 @@ public class AddTaskActivity extends Activity implements LocationListener {
 		setContentView(R.layout.add_task);
 		
 		if (savedInstanceState == null) {
-			getParametersFromBundle(getIntent().getExtras());
+			if (getBundle() != null) {
+				getParametersFromBundle(getBundle());
+			}
 		}
 		else {
-			index = savedInstanceState.getInt("index");
-			latitude = savedInstanceState.getDouble("latitude");
-			longitude = savedInstanceState.getDouble("longitude");
+			getParametersFromSavedInstanceState(savedInstanceState);
 		}
 		
 		refreshUi();
-	}
-
-
-	private void getParametersFromBundle(Bundle bundle) {
-		if (bundle != null) {
-			TaskItem item = bundle.getParcelable("item");
-			if( item != null ) {
-				text = item.getText();
-				
-				latitude = item.getLatitude();
-				longitude = item.getLongitude();
-				
-				index = bundle.getInt("index");
-			}
-		}
-	}
-
-
-	private void refreshUi() {
-		Button addTaskButton = (Button) findViewById(R.id.add_task_button);
-		EditText editText = (EditText) findViewById(R.id.new_task_text);
-
-		editText.setText(text);
-		TextView locationView = (TextView) AddTaskActivity.this.findViewById(R.id.location_display_text);
-		locationView.setText("latitude: " + latitude + " - longitude: " + longitude);
-		
-		addTaskButton.setOnClickListener(listener);
-		
-		setCurrentGpsLocation(null);
-		
-		Button setMyLocationButton = (Button) AddTaskActivity.this.findViewById(R.id.set_current_location_button);
-		setMyLocationButton.setOnClickListener(setMyLocationButtonListener);
 	}
 
 	@Override
-	public void setIntent (Intent newIntent) {
-		super.setIntent(newIntent);
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
 		
-		getParametersFromBundle(newIntent.getExtras());
-		refreshUi();
-	}
-    
-    
-	OnClickListener setMyLocationButtonListener = new OnClickListener() {
-		public void onClick(View v) {
-			setCurrentGpsLocation(null);
-			
-			checkGPSAvailability();
-			
-			longitude = currentLongitude;
-			latitude = currentLatitude;
-			
-			TextView locationView = (TextView) AddTaskActivity.this.findViewById(R.id.location_display_text);
-			locationView.setText("latitude: " + latitude + " - longitude: " + longitude);
-		}
-
-		private void checkGPSAvailability() {
-			if (currentLatitude == null || currentLongitude == null) {
-				Toast toast = Toast.makeText(getApplicationContext(), R.string.gps_availability_alert, Toast.LENGTH_SHORT);
-				toast.show();
-			}
-		}
-	};
-	
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-    	super.onSaveInstanceState(outState);
-    	
 		outState.putInt("index", index);
 		outState.putDouble("latitude", latitude);
 		outState.putDouble("longitude", longitude);
-    }
-	
-	private OnClickListener listener = new OnClickListener() {
-		public void onClick(View v) {
-			Intent data = new Intent();
-			
-			EditText editText = (EditText) findViewById(R.id.new_task_text);
-			String text = editText.getText().toString();
-			data.putExtra("newTaskText", text);
-			data.putExtra("latitude", latitude);
-			data.putExtra("longitude", longitude);
-			data.putExtra("index", index);
-
-			AddTaskActivity.this.setResult(Activity.RESULT_OK, data);
-			AddTaskActivity.this.finish();
-		}
-	};
-	
-	private int index = -1;
-	private String text = "";
-	private Double currentLatitude = null;
-	private Double currentLongitude = null;
-	private Double latitude;
-	private Double longitude;
-
-	// GPS management
-	private LocationManager locationManager;
-
-	private void setCurrentGpsLocation(Location location) {
-		if (location == null) {
-			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
-			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-			location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		}
-		if( location != null ) {
-			currentLatitude = location.getLatitude();
-			currentLongitude = location.getLongitude();
-		} else {
-			currentLatitude = null;
-			currentLongitude = null;
-		}
 	}
-	
+
 	public void onLocationChanged(Location location) {
 		setCurrentGpsLocation(location);
 	}
@@ -163,4 +61,122 @@ public class AddTaskActivity extends Activity implements LocationListener {
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		setCurrentGpsLocation(null);
 	}
+
+	@Override
+	public void setIntent (Intent newIntent) {
+		super.setIntent(newIntent);
+		
+		getParametersFromBundle(newIntent.getExtras());
+		refreshUi();
+	}
+
+	private Bundle getBundle() {
+		return getIntent().getExtras();
+	}
+	
+	private void getParametersFromSavedInstanceState(Bundle savedInstanceState) {
+		index = savedInstanceState.getInt("index");
+		latitude = savedInstanceState.getDouble("latitude");
+		longitude = savedInstanceState.getDouble("longitude");
+	}
+
+	private void getParametersFromBundle(Bundle bundle) {
+		TaskItem item = bundle.getParcelable("item");
+		if( item != null ) {
+			text = item.getText();
+			
+			latitude = item.getLatitude();
+			longitude = item.getLongitude();
+			
+			index = bundle.getInt("index");
+		}
+	}
+
+	private void refreshUi() {
+		Button addTaskButton = (Button) findViewById(R.id.add_task_button);
+		EditText taskText = (EditText) findViewById(R.id.new_task_text);
+
+		taskText.setText(text);
+		TextView locationView = (TextView) AddTaskActivity.this.findViewById(R.id.location_display_text);
+		locationView.setText("latitude: " + latitude + " - longitude: " + longitude);
+		
+		addTaskButton.setOnClickListener(okButtonListener);
+		
+		setCurrentGpsLocation(null);
+		
+		Button setMyLocationButton = (Button) AddTaskActivity.this.findViewById(R.id.set_current_location_button);
+		setMyLocationButton.setOnClickListener(setMyLocationButtonListener);
+	}
+
+	private void setCurrentGpsLocation(Location location) {
+		updateLocationManagerIfNeeded(location);
+		location = getLastKnownPosition();
+		
+		if( location != null ) 
+			setCurrentLocation(location.getLatitude(), location.getLongitude()); 
+		else 
+			setCurrentLocation(null, null);
+	}
+	
+	private void setCurrentLocation(Double latitude, Double longitude) {
+		currentLatitude = latitude;
+		currentLongitude = longitude;
+	}
+
+	private Location getLastKnownPosition() {
+		return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	}
+
+	private void updateLocationManagerIfNeeded(Location location) {
+		if (location == null) {
+			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		}
+	}
+	
+	private OnClickListener setMyLocationButtonListener = new OnClickListener() {
+		public void onClick(View v) {
+			setCurrentGpsLocation(null);
+			
+			checkGPSAvailability();
+			
+			longitude = currentLongitude;
+			latitude = currentLatitude;
+			
+			TextView locationView = (TextView) AddTaskActivity.this.findViewById(R.id.location_display_text);
+			locationView.setText("latitude: " + latitude + " - longitude: " + longitude);
+		}
+	
+		private void checkGPSAvailability() {
+			if (currentLatitude == null || currentLongitude == null) {
+				Toast toast = Toast.makeText(getApplicationContext(), R.string.gps_availability_alert, Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		}
+	};
+	
+	private OnClickListener okButtonListener = new OnClickListener() {
+		public void onClick(View v) {
+			Intent data = new Intent();
+			
+			EditText taskText = (EditText) findViewById(R.id.new_task_text);
+			String text = taskText.getText().toString();
+			data.putExtra("newTaskText", text);
+			data.putExtra("latitude", latitude);
+			data.putExtra("longitude", longitude);
+			data.putExtra("index", index);
+	
+			AddTaskActivity.this.setResult(Activity.RESULT_OK, data);
+			AddTaskActivity.this.finish();
+		}
+	};
+	
+	private int index = -1;
+	private String text = "";
+	private Double currentLatitude = null;
+	private Double currentLongitude = null;
+	private Double latitude;
+	private Double longitude;
+
+	private LocationManager locationManager;
 }
