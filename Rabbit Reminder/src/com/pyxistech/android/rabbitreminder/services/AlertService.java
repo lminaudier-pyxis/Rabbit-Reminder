8661f2c894044e2849a42c21c2a14eb9fb7ac7c4
Loic GIRON DIT METAZ <lgironditmetaz@pyxis-tech.com>
2010-06-23 17:57:41 +0200

    Bug fix: the alert thread is now stopped when the alert service is stopped

--- Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/services/AlertService.java
+++ Rabbit Reminder/src/com/pyxistech/android/rabbitreminder/services/AlertService.java
@@ -23,134 +23,14 @@ import com.pyxistech.android.rabbitreminder.models.AlertList;
 
 public class AlertService extends Service {
 
-	Thread notificationThread;
+	AlertThread notificationThread;
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		
 		if (notificationThread == null) {
-			
-			notificationThread = new Thread( new Runnable() {
-
-			    private final String[] PROJECTION = new String[] {
-			        AlertList.Items._ID, // 0
-			        AlertList.Items.NAME, // 1
-			        AlertList.Items.DONE, // 2
-			        AlertList.Items.LATITUDE, // 3
-			        AlertList.Items.LONGITUDE // 4
-			    };
-			    
-				public void run() {
-					Looper.prepare();
-					
-					while (true) {
-						Location myLocation = getLocation();
-						Vector<AlertItem> tasks = getUndoneTasks();
-						Vector<AlertItem> localTasks = new Vector<AlertItem>();
-						
-						if (myLocation != null) {
-							for (AlertItem taskItem : tasks) {
-								Location taskItemLocation = buildLocationFromTaskItem(taskItem);
-								
-								if (isTaskLocationNearMyLocation(myLocation, taskItemLocation)) {
-									localTasks.add(taskItem);
-								}
-							}
-							
-							if (localTasks.size() > 0) {
-								Intent intent = buildNotificationIntent(localTasks);
-								notifyUser(localTasks.size(), intent);
-								threadWait(10000);
-							}
-						}
-
-						threadWait(1000);
-					}
-				}
-
-				private Intent buildNotificationIntent(Vector<AlertItem> taskItems) {
-					Intent intent = new Intent(AlertService.this, AlertActivity.class);
-//					intent.putExtra("index", (int) -1);
-//					intent.putExtra("item", taskItem);
-					return intent;
-				}
-
-				private boolean isTaskLocationNearMyLocation(Location myLocation, Location taskItemLocation) {
-					return taskItemLocation.distanceTo(myLocation) < 100;
-				}
-
-				private Location buildLocationFromTaskItem(AlertItem taskItem) {
-					Location taskItemLocation = new Location("com.pyxistech.android.rabbitreminder.providers.TaskListProvider");
-					taskItemLocation.setLatitude(taskItem.getLatitude());
-					taskItemLocation.setLongitude(taskItem.getLongitude());
-					return taskItemLocation;
-				}
-
-				private void notifyUser(int taskItemsSize, Intent notificationIntent) {					
-					NotificationManager nm = (NotificationManager) AlertService.this.getSystemService(Context.NOTIFICATION_SERVICE);
-					Notification notification = new Notification(R.drawable.icon, "You have to accomplish a task here!", System.currentTimeMillis());
-					PendingIntent pendingIntent = PendingIntent.getActivity(AlertService.this, 0, notificationIntent, 0);
-					notification.setLatestEventInfo(AlertService.this, "There is " + taskItemsSize + " local tasks", "", pendingIntent);
-
-					nm.notify(0, notification);
-				}
-
-				private void threadWait(int time) {
-					try {
-						Thread.sleep(time);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-
-				private Vector<AlertItem> getUndoneTasks() {
-					Vector<AlertItem> tasks = new Vector<AlertItem>();
-					
-					Cursor tasksCursor = getContentResolver().query(AlertList.Items.CONTENT_URI, 
-		    				PROJECTION, undoneTaskWhereClause(), null, 
-		    				AlertList.Items.DEFAULT_SORT_ORDER);
-					
-					if( tasksCursor.moveToFirst() ) {
-						do {
-							tasks.add(new AlertItem(
-										Integer.valueOf(tasksCursor.getString(0)), 
-										tasksCursor.getString(1), 
-										tasksCursor.getInt(2) == 1, 
-										tasksCursor.getDouble(3), 
-										tasksCursor.getDouble(4)
-								));
-				        } while(tasksCursor.moveToNext());
-			        }
-					
-					return tasks;
-				}
-
-				private String undoneTaskWhereClause() {
-					return AlertList.Items.DONE + "=0";
-				}
-				
-				private Location getLocation() {
-					LocationManager lm = (LocationManager) AlertService.this.getSystemService(Context.LOCATION_SERVICE);
-					lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-					return lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-				}
-				
-				public LocationListener locationListener = new LocationListener() {
-					public void onStatusChanged(String provider, int status, Bundle extras) {
-					}
-					
-					public void onProviderEnabled(String provider) {
-					}
-					
-					public void onProviderDisabled(String provider) {
-					}
-					
-					public void onLocationChanged(Location location) {
-					}
-				};
-			});
-			
+			notificationThread = new AlertThread(this);
 			notificationThread.start();
 		}
 	}
@@ -159,7 +39,7 @@ public class AlertService extends Service {
 	public void onDestroy() {
 		super.onDestroy();
 		
-		notificationThread.interrupt();
+		notificationThread.interrupted = true;
 		notificationThread = null;
 	}
 	
@@ -174,4 +54,130 @@ public class AlertService extends Service {
 		return null;
 	}
 
+}
+
+class AlertThread extends Thread {
+	public boolean interrupted = false;
+	private AlertService context;
+
+    private final String[] PROJECTION = new String[] {
+        AlertList.Items._ID, // 0
+        AlertList.Items.NAME, // 1
+        AlertList.Items.DONE, // 2
+        AlertList.Items.LATITUDE, // 3
+        AlertList.Items.LONGITUDE // 4
+    };
+    
+    public AlertThread(AlertService context) {
+    	this.context = context;
+    }
+    
+	public void run() {
+		Looper.prepare();
+		
+		while (!interrupted) {
+			Location myLocation = getLocation();
+			Vector<AlertItem> tasks = getUndoneTasks();
+			Vector<AlertItem> localTasks = new Vector<AlertItem>();
+			
+			if (myLocation != null) {
+				for (AlertItem taskItem : tasks) {
+					Location taskItemLocation = buildLocationFromTaskItem(taskItem);
+					
+					if (isTaskLocationNearMyLocation(myLocation, taskItemLocation)) {
+						localTasks.add(taskItem);
+					}
+				}
+				
+				if (localTasks.size() > 0) {
+					Intent intent = buildNotificationIntent(localTasks);
+					notifyUser(localTasks.size(), intent);
+					threadWait(10000);
+				}
+			}
+
+			threadWait(1000);
+		}
+	}
+
+	private Intent buildNotificationIntent(Vector<AlertItem> taskItems) {
+		Intent intent = new Intent(context, AlertActivity.class);
+//		intent.putExtra("index", (int) -1);
+//		intent.putExtra("item", taskItem);
+		return intent;
+	}
+
+	private boolean isTaskLocationNearMyLocation(Location myLocation, Location taskItemLocation) {
+		return taskItemLocation.distanceTo(myLocation) < 100;
+	}
+
+	private Location buildLocationFromTaskItem(AlertItem taskItem) {
+		Location taskItemLocation = new Location("com.pyxistech.android.rabbitreminder.providers.TaskListProvider");
+		taskItemLocation.setLatitude(taskItem.getLatitude());
+		taskItemLocation.setLongitude(taskItem.getLongitude());
+		return taskItemLocation;
+	}
+
+	private void notifyUser(int taskItemsSize, Intent notificationIntent) {					
+		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notification = new Notification(R.drawable.icon, "You have to accomplish a task here!", System.currentTimeMillis());
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(context, "There is " + taskItemsSize + " local tasks", "", pendingIntent);
+
+		nm.notify(0, notification);
+	}
+
+	private void threadWait(int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Vector<AlertItem> getUndoneTasks() {
+		Vector<AlertItem> tasks = new Vector<AlertItem>();
+		
+		Cursor tasksCursor = context.getContentResolver().query(AlertList.Items.CONTENT_URI, 
+				PROJECTION, undoneTaskWhereClause(), null, 
+				AlertList.Items.DEFAULT_SORT_ORDER);
+		
+		if( tasksCursor.moveToFirst() ) {
+			do {
+				tasks.add(new AlertItem(
+							Integer.valueOf(tasksCursor.getString(0)), 
+							tasksCursor.getString(1), 
+							tasksCursor.getInt(2) == 1, 
+							tasksCursor.getDouble(3), 
+							tasksCursor.getDouble(4)
+					));
+	        } while(tasksCursor.moveToNext());
+        }
+		
+		return tasks;
+	}
+
+	private String undoneTaskWhereClause() {
+		return AlertList.Items.DONE + "=0";
+	}
+	
+	private Location getLocation() {
+		LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+		return lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	}
+	
+	public LocationListener locationListener = new LocationListener() {
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+		
+		public void onProviderEnabled(String provider) {
+		}
+		
+		public void onProviderDisabled(String provider) {
+		}
+		
+		public void onLocationChanged(Location location) {
+		}
+	};
 }
